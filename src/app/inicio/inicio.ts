b8992f9792fac9897c21e3e450e0238b74ca94b7
Luiz_Vital <luizvital832@gmail.com>
2026-08-08 21:59:43 -0300

    Fim do Visual

--- src/app/inicio/inicio.ts
+++ src/app/inicio/inicio.ts
@@ -1,13 +1,11 @@
 import { Component } from '@angular/core';
-import { Menu } from '../menu/menu';
-import { Rodape } from '../rodape/rodape';
 import { MatButtonModule } from  '@angular/material/button' ;
 import { MatCardModule } from  '@angular/material/card' ;
 import {MatGridListModule} from '@angular/material/grid-list';
 
 @Component({
   selector: 'app-inicio',
-  imports: [ MatCardModule, MatButtonModule, MatGridListModule, Menu, Rodape ],
+  imports: [ MatCardModule, MatButtonModule, MatGridListModule ],
   templateUrl: './inicio.html',
   styleUrl: './inicio.css',
 })
--- src/app/login/login.ts
+++ src/app/login/login.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { Menu } from '../menu/menu';
-import { Rodape } from '../rodape/rodape';
 
 @Component({
   selector: 'app-login',
-  imports: [ Menu, Rodape ],
+  imports: [],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
